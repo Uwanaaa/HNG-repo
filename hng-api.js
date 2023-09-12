7c6282768919a697a71827ce98d0868dbe2e4c9a
Udofia Unwana <121640800+Uwanaaa@users.noreply.github.com>
2023-09-12 12:21:58 +0100

    Update

--- hng-api.js
+++ hng-api.js
@@ -3,12 +3,37 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 
+const day = new Date.getDay()
+function today(){
+    switch (day) {
+  case 0:
+    return "Sunday";
+  case 1:
+    return "Monday";
+  case 2:
+    return "Tuesday";
+  case 3:
+    return "Wednesday";
+  case 4:
+    return "Thursday";
+  case 5:
+    return "Friday";
+  case  6:
+    return "Saturday";
+}
+};
+
+const day = new Date();
+const timeWithoutMilliseconds = new Date(day.setMilliseconds(0)).toISOString().slice(0, -5) + 'Z';
+
+console.log(utcTimeWithoutMilliseconds);
+
 app.get('/project1', (req, res) => {
     return res.json({
-        "slack_name": "example_name",
-        "current_day": "Monday",
-        "utc_time": "2023-08-21T15:04:05Z",
-        "track": "backend",
+        "slack_name": "req.query.slack_name",
+        "current_day": today(),
+        "utc_time": timeWithoutMilliseconds,
+        "track": "req.query.track",
         "github_file_url": "https://github.com/Uwanaaa/repo/blob/main/file_name.ext",
         "github_repo_url": "https://github.com/Uwanaaa/HNG-repo.git",
         "status_code": 200
